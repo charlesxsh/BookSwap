f55f1eb573a731b6fddaec94032f7ad18f6a50dc
Charles Xia <charlesxsh@hotmail.com>
2016-03-01 23:24:59 -0500

    update

--- BookSwap-web/public/js/bookdetail.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-		console.log('ready!!');
-		$('#title').text(bookDetail.title);
-		$('#edition').text(bookDetail.edition);
-		$('#author').text(bookDetail.author);
-	}
-);
--- BookSwap-web/public/js/userprofile.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-		console.log('ready!!');
-		populateBookFlow(Adapter.BookItem('Computer Science: A Better Introduction','Me'));
-		addListeners();
-		sizeAdapter();
-	}
-);
